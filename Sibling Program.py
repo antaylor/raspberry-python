624777b274fd49f477dc8edbf99c7ed821875f7d
Andrew Taylor <antaylorcfc@gmail.com>
2016-07-05 12:10:57 -0400

    Add files via upload

---
+++ Sibling Program.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name: Sibling Program
+# Purpose:State ages and names
+# Author: Andrew Taylor
+# Created:     07/05/2016
+# Copyright:   (c) Andrew 2016
+#------------------------------------------------------------------------------
+
+j = 'jessup'
+k = 'kate'
+a = 'andrew'
+r = 'reese'
+
+
+
+jessup = "Eldest son, Male, 38 years old"
+kate = "Oldest daughter, Female, 29 years old"
+andrew = "Second son, Male, 28 years old"
+reese = "Third son and youngest child, Male, 22 years old"
+
+
+
+J = 38
+K = 29
+A = 28
+R = 22
